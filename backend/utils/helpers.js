da16c3011bc700ef4be047ac6ed09c501748214a
Abrham Kindie <abrhamkindie805@gmail.com>
2025-04-14 23:17:40 +0300

    updated handlejobseeker

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,11 +1,22 @@
 
-exports.insertIntoDatabase = (table, data, res,db) => {
-  db.query(`INSERT INTO ${table} SET ?`, data, (err, result) => {
-    if (err) {
-      console.error(`Error inserting data into ${table}:`, err);
-      return res.status(500).json({ error: `Error inserting data into ${table}`, details: err.message });
-    }
-    res.json({ message: `${table.charAt(0).toUpperCase() + table.slice(1)} created successfully` });
-  });
-};
+// exports.insertIntoDatabase = (table, data, res,db) => {
+//   db.query(`INSERT INTO ${table} SET ?`, data, (err, result) => {
+//     if (err) {
+//       console.error(`Error inserting data into ${table}:`, err);
+//       return res.status(500).json({ error: `Error inserting data into ${table}`, details: err.message });
+//     }
+//     res.json({ message: `${table.charAt(0).toUpperCase() + table.slice(1)} created successfully` });
+//   });
+// };
  
+
+// utils/helpers.js
+exports.insertIntoDatabase = async (table, data, db) => {
+  try {
+    const [result] = await db.promise().query(`INSERT INTO ${table} SET ?`, data);
+    return { insertId: result.insertId };
+  } catch (err) {
+    console.error(`❌ Error inserting data into ${table}:`, err);
+    throw err;
+  }
+};
